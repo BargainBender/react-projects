bae3f6f6272bd250169db39d971f9655c3257894
Sebastian Martinez <bargainbend@gmail.com>
2021-01-20 06:36:18 +0800

    Reviews done

--- 2-tours/setup/src/Tour.js
+++ 2-tours/setup/src/Tour.js
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-const Tour = () => {
-  return <h2>tour component</h2>;
-};
+const Tour = ({ id, image, info, price, name, removeTour }) => {
+	const [isExpanded, setIsExpanded] = useState(false)
+	return (
+		<article className='single-tour'>
+			<img src={image} alt={name} />
+			<footer>
+				<div className='tour-info'>
+					<h4>{name}</h4>
+					<h4 className='tour-price'>${price}</h4>
+				</div>
+				<p>
+					{isExpanded ? info : `${info.substring(0, 200)}...`}
+					<button onClick={() => setIsExpanded(!isExpanded)}>
+						{isExpanded ? 'Show less' : 'Read More'}
+					</button>
+				</p>
+				<button className='delete-btn' onClick={() => removeTour(id)}>
+					Not interested
+				</button>
+			</footer>
+		</article>
+	)
+}
 
-export default Tour;
+export default Tour
